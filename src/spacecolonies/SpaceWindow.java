5bbba874ed3d8dc9a3bb14826ba9cbce73af9267
aliparslan <40613474+aliparslan@users.noreply.github.com>
2021-10-27 11:15:48 -0400

    Update SpaceWindow.java

--- src/spacecolonies/SpaceWindow.java
+++ src/spacecolonies/SpaceWindow.java
@@ -1,9 +1,3 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Alip Arslan (906347003)
 package spacecolonies;
 
 import java.awt.Color;
